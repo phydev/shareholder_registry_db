1f4eb2be76cf999979040368cfdbc86a111a9233
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2026-09-05 11:48:48 +0200

    prune infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,25 +22,3 @@ resource "docker_image" "registry_image" {
   keep_locally = false
 }
 
-resource "docker_image" "minio_image" {
-  name         = "quay.io/minio/aistor/minio:RELEASE.2026-08-07T18-34-35Z"
-  keep_locally = true
-}
-
-resource "docker_image" "unitycatalog_image" {
-  name         = "unitycatalog/unitycatalog"
-  keep_locally = true
-}
-
-resource "null_resource" "build_ui_with_volume" {
-  provisioner "local-exec" {
-    command = <<EOT
-      mkdir -p ${var.home_dir}/.podman-build-cache
-
-      podman build \
-        -v  ${var.home_dir}/.podman-build-cache:/var/lib/containers/storage/overlay-layers/tmp:O \
-        -t localhost/unitycatalog-ui:latest \
-        ${var.unitycatalog_ui_path}
-    EOT
-  }
-}
