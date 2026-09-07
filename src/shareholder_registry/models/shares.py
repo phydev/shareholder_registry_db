919ba9eed9d49994efeedf4c79ad776321c4b4f0
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2026-09-07 23:39:42 +0200

    fix biginteger definition

--- src/shareholder_registry/models/shares.py
+++ src/shareholder_registry/models/shares.py
@@ -1,7 +1,7 @@
 import uuid
 from uuid import UUID
 
-from sqlmodel import Field, Relationship, SQLModel, BigInteger
+from sqlmodel import Field, Relationship, SQLModel, Column, BigInteger
 
 
 class Shares(SQLModel, table=True):
@@ -11,8 +11,8 @@ class Shares(SQLModel, table=True):
 
     year: str = Field(max_length=4)
     share_class: str = Field(max_length=100)
-    shares_owned: BigInteger
-    total_shares_in_company: BigInteger
+    shares_owned: int = Field(sa_column=Column(BigInteger))
+    total_shares_in_company: int = Field(sa_column=Column(BigInteger))
 
     part: "Part" = Relationship(back_populates="investments") # noqa: F821
     company: "Company" = Relationship(back_populates="shareholders") # noqa: F821
